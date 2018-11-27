a48f85b8e44922b5b5f29a9faa83b2cb79d83860
Alex Claude <abauruel@gmail.com>
2018-11-27 01:39:46 -0200

    adicionado listagem de agendamentos

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,9 @@
 module.exports = {
   dialect: "mysql",
-  host: "localhost",
-  username: "root",
-  password: "root",
+  host: "127.0.0.1",
+  port: 55555,
+  username: "gonode",
+  password: "gonode",
   database: "gonodemodulo2",
   operatorAliases: false,
   define: {
